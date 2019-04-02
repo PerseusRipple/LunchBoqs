107570f4579443e946215e7f25b1e8e723bd8183
PerseusRipple <perseus.ripple@gmail.com>
2019-04-02 17:00:09 -0400

    css calendar, got rid of inherit

--- ClientApp/src/pages/Plan.js
+++ ClientApp/src/pages/Plan.js
@@ -5,32 +5,47 @@ class Plan extends Component {
   render() {
     return (
       <section>
+        <nav>
+          <h1 className='calendar-header'>Your LunchBoqs</h1>
+        </nav>
         <section className='calendar-container'>
           <section className='calendar'>
-            <h1 className='calendar-header'>Your Meal Plan</h1>
-
-            <table width='400' cellPadding='10' cellSpacing='5'>
-              <tr className='days-rows'>
-                <th width='150' />
-                <th>Monday</th>
-                <th>Tuesday</th>
-                <th>Wednesday</th>
-                <th>Thursday</th>
-                <th width='150'>Friday</th>
-              </tr>
-              <tr className='lunch-row'>
-                <th>Lunch</th>
-                {/* <td rowspan='2'>Yummy</td> */}
-              </tr>
-              <tr className='snack-row'>
-                <th>Snack</th>
-              </tr>
+            <table>
+              <thead>
+                <tr className='days-rows'>
+                  <th />
+                  <th>Monday</th>
+                  <th>Tuesday</th>
+                  <th>Wednesday</th>
+                  <th>Thursday</th>
+                  <th>Friday</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr className='lunch-row'>
+                  <td>Lunch</td>
+                  <td />
+                  <td />
+                  <td />
+                  <td />
+                  <td />
+                </tr>
+                <tr className='snack-row'>
+                  <td>Snack</td>
+                  <td />
+                  <td />
+                  <td />
+                  <td />
+                  <td />
+                </tr>
+              </tbody>
             </table>
           </section>
 
           {/* <h3>Select the days you want a LunchBoqs</h3>
         <h4>Click 'Pack' to view your local menu options</h4> */}
         </section>
+        <section className='mid-container'>Restaurant Images Go Here</section>{' '}
         <section>
           <button className='step-two' type='submit'>
             Pack
